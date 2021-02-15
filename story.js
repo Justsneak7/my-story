12c97f4379476153bb87b241f919f4505611ecb6
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2021-02-15 20:55:47 +0530

    Update story.js

--- story.js
+++ story.js
@@ -2,3 +2,4 @@ One day there was a Monkey who wanted to be friends with a Crocodile. Monkey a l
 In the same forest lived a Crocodile with his Wife. 
 One day the Crocodile came to the banks of the river and rested under the tree. The kind hearted Monkey offered some fruits. The next
 day the Crocodile came for more fruits and as the days passed by the Crocodile and the Monkey became friends. The Monkey was happy.
+They finally reached their destination.
