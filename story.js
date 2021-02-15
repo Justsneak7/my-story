98dad126be1f1a59f7f159f53891d5b3217ad492
justsneak7 <slashsneak7@gmail.com>
2021-02-15 20:27:09 +0530

    VERSION_2

---
+++ story.js
@@ -0,0 +1,4 @@
+One day there was a Monkey who wanted to be friends with a Crocodile. Monkey a liven on a jamun tree which was on the bank of the river.
+In the same forest lived a Crocodile with his Wife. 
+One day the Crocodile came to the banks of the river and rested under the tree. The kind hearted Monkey offered some fruits. The next
+day the Crocodile came for more fruits and as the days passed by the Crocodile and the Monkey became friends.
